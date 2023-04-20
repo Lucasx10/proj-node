2f72c42b45ade8aa64a1c5ec769fea42a4219ff9
Lucasx10 <lukasprado260@gmail.com>
2023-04-20 08:22:02 -0400

    first commit

---
+++ proj-node-main/app.js
@@ -0,0 +1,19 @@
+import express from "express";
+import routes from "./routes/index.js";
+import sequelize from "./config/sequelize.js";
+
+const app = express();
+const PORT = 3000;
+
+// MIDDLEWARE
+app.use(express.json());
+
+// ROTAS
+app.use("/", routes);
+
+// Conexão com Banco de Dados
+sequelize.sync().then(() => {
+  console.log("Conectado ao Banco de Dados com sucesso!");
+});
+
+app.listen(PORT, () => console.log("Servidor rodando na porta " + PORT));
